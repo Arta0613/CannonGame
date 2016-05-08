a2536c9f1b4bb39e69775ab0cc4a6521834b8d0f
Mohammed Amin <arta0613@gmail.com>
2016-05-08 00:58:00 -0400

    Set up project

---
+++ app/src/main/java/com/rushlimit/cannongame/CannonView.java
@@ -0,0 +1,14 @@
+package com.rushlimit.cannongame;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.SurfaceView;
+
+/**
+ * Created by Arman on 5/8/16.
+ */
+public class CannonView extends SurfaceView {
+    public CannonView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
